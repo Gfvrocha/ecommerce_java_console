854529671d357036e7273d858990beb3b16b6d02
Felipe Rocha <felipe.viega.r@gmail.com>
2023-04-10 11:38:50 -0300

    Exercício 1

--- src/main/java/br/com/felipe_java/App.java
+++ src/main/java/br/com/felipe_java/App.java
@@ -1,15 +1,40 @@
 package br.com.felipe_java;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        int n1 = 5;
-        var n2 = 1;
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
 
-        int soma = n1 + n2;
+        /*
+         * Exercício 1
+         * João é um professor de matematica e precisa de um programa que faça
+         * o calculo da seguinte expressão:
 
-        System.out.println( "Hello World!" );
+         *  (X * 8 + Y) - X + Y / 100
+         * 
+         */
+        System.out.println("===========================");
+        System.out.println("\t BEM-VINDO");
+        System.out.println("===========================");
+        System.out.println("João, vamos resolver o seu problema");
+        System.out.println("Vou precisar de algumas informações para a expressão (X * 8 + Y) - X + Y / 100");
+
+        System.out.println("Digite o valor de X:" );
+        int x = Integer.parseInt(reader.readLine());
+
+        System.out.println("Digite o valor de Y:" );
+        int y = Integer.parseInt(reader.readLine());
+
+        var operacao = (x * 8 + y) - x + y / 100;
+
+        System.out.println("Resolvendo => ( " + x + " * 8 + " + y + ") - " + x + " + " + y + "/ 100");
+        System.out.println("Resultado: " + operacao);
+
     }
 }
